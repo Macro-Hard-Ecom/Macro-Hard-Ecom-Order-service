dec5df48e5528e53ca24477cc3774904da9652a3
ishini21 <ishinishavindhya@gmail.com>
2026-03-21 22:36:05 +0530

    updated

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -14,7 +14,21 @@ const {
  *     tags: [Orders]
  */
 router.post('/createOrder', createOrder);
+/**
+ * @swagger
+ * /api/orders/orders:
+ *   get:
+ *     summary: Get orders
+ *     tags: [Orders]
+ */
 router.get('/orders', getOrders);
+/**
+ * @swagger
+ * /api/orders/order/:id:
+ *   post:
+ *     summary: Get order by Id
+ *     tags: [Orders]
+ */
 router.get('/order/:id', getOrderById);
 
 module.exports = router;

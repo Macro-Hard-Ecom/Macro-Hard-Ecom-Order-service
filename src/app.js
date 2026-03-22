8e7f6df04ed8d004138b67c677d31f4198d43622
ishini21 <ishinishavindhya@gmail.com>
2026-03-22 09:15:45 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const orderRoutes = require('./routes/orderRoutes');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(helmet());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/orders', orderRoutes);

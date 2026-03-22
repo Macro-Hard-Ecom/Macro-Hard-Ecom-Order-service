1aed0cafea0ec9c48714dbbc9c89bd13597363a1
ishini21 <ishinishavindhya@gmail.com>
2026-03-22 07:11:11 +0530

    add url

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const PRODUCT_SERVICE_URL = "http://localhost:3000"; // change later to AWS IP
+const PRODUCT_SERVICE_URL = "http://98.81.229.185:3000"; 
 
 const getProduct = async (id) => {
   const res = await axios.get(`${PRODUCT_SERVICE_URL}/products/${id}`);

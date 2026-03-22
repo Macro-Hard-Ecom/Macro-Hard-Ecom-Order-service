369275049df410ebc1d6ed374621f19c66a1df9d
ishini21 <ishinishavindhya@gmail.com>
2026-03-22 11:33:52 +0530

    updated

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,18 +1,22 @@
 const Order = require('../models/order.js');
 const { validateUser } = require('../services/userService');
 const { getProduct } = require('../services/productService');
+const jwt = require('jsonwebtoken');
 
 exports.createOrder = async (req, res) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
-    const user = await validateUser(token);
+   
+    await validateUser(token);
+
+    const decoded = jwt.decode(token);
+    const userId = decoded.sub;
 
     let total = 0;
     const items = [];
 
     for (let item of req.body.items) {
-      
       const product = await getProduct(item.productId, token);
 
       items.push({
@@ -22,27 +26,19 @@ exports.createOrder = async (req, res) => {
         quantity: item.quantity,
       });
 
-      total += product.price * item.quantity;
+      total += product.price * item.quantity; 
     }
 
     const order = await Order.create({
-      userId: user.id,
+      userId, 
       items,
-      totalAmount: total,
+      totalAmount: total, 
     });
 
     res.status(201).json(order);
+
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
-};
-
-exports.getOrders = async (req, res) => {
-  const orders = await Order.find();
-  res.json(orders);
-};
-
-exports.getOrderById = async (req, res) => {
-  const order = await Order.findById(req.params.id);
-  res.json(order);
 };

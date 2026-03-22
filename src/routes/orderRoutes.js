4e024766608455adead38a7393dd13d9b0536f51
ishini21 <ishinishavindhya@gmail.com>
2026-03-22 10:14:01 +0530

    updated swagger

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -8,26 +8,59 @@ const {
 
 /**
  * @swagger
+ * tags:
+ *   name: Orders
+ *   description: Order management
+ */
+
+/**
+ * @swagger
  * /api/orders/createOrder:
  *   post:
  *     summary: Create new order
  *     tags: [Orders]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             items:
+ *               - productId: "123"
+ *                 quantity: 2
+ *     responses:
+ *       201:
+ *         description: Order created successfully
  */
 router.post('/createOrder', createOrder);
+
 /**
  * @swagger
  * /api/orders/orders:
  *   get:
- *     summary: Get orders
+ *     summary: Get all orders
  *     tags: [Orders]
+ *     responses:
+ *       200:
+ *         description: List of orders
  */
 router.get('/orders', getOrders);
+
 /**
  * @swagger
- * /api/orders/order/:id:
+ * /api/orders/order/{id}:
  *   get:
- *     summary: Get order by Id
+ *     summary: Get order by ID
  *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Order ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Order details
  */
 router.get('/order/:id', getOrderById);
 
